2b2aa69150d0d31c2e1d164ee6b6fee8f91b2010
juanmcr06 <juanmcr.06@gmail.com>
2022-08-10 04:31:57 -0300

    update

---
+++ js/01.js
@@ -0,0 +1,21 @@
+// VARIABLES
+
+var producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+
+var disponible; // Iniciar variable sin asignar valor
+
+// producto = true; // Reasignar un valor a la misma variable antes utilizada
+
+
+var producto1 = 'Computadora',  // Iniciar mas de una variable en una misma linea, separadas por una ","
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// ESTILOS DE VARIABLES MAS UTILIZADOS
+
+var nombre_produco = 'Monitor HD'; // UNDERSCORE
+var nombreProducto = 'Monitor HD'; // CAMELCASE     Mas Utilizado!
+var NombreProducto = 'Monitor HD'; // PASCALCASE
+var nombreproducto = 'Monitor HD'; // LOWERCASE
+
+console.log(producto);
---
+++ js/02.js
@@ -0,0 +1,19 @@
+// VARIABLES CON LET
+
+let producto = 'Audifonos Gamer'; // Iniciar variable y asignarle un valor
+
+let disponible; // Iniciar variable sin asignar valor
+
+// producto = true; // Reasignar un valor a la misma variable antes utilizada
+
+
+let producto1 = 'Computadora',  // Iniciar mas de una variable en una misma linea, separadas por una ","
+    disponible1 = true,
+    categoria = 'Computadoras';
+
+// ESTILOS DE VARIABLES MAS UTILIZADOS
+
+let nombre_produco = 'Monitor HD'; // UNDERSCORE
+let nombreProducto = 'Monitor HD'; // CAMELCASE     Mas Utilizado!
+let NombreProducto = 'Monitor HD'; // PASCALCASE
+let nombreproducto = 'Monitor HD'; // LOWERCASE
