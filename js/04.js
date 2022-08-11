e50e07aceb98b82e7a5182b1f0ed2cc69f5b97ca
juanmcr06 <juanmcr.06@gmail.com>
2022-08-11 04:57:35 -0300

    update

---
+++ js/04.js
@@ -0,0 +1,19 @@
+// STRINGS O CADENAS DE TEXTO
+
+const producto = 'Aprendiendo JavaScript con el curso de Desarrollo Web Completo'; // Manera tradicional. EL MAYORMENTE UTILIZADO
+
+const producto2 = 'Monitor HD';
+
+// length es para saber longitud de caracteres
+console.log(producto.length);
+
+// indexOF es para saber si existe en el String la palabra que buscamos
+
+console.log(producto.indexOf('JavaScript'));
+console.log(producto.indexOf('Tablet'));
+
+// includes puede buscar la palabra que indicamos y retorna como booleano el resultado
+
+console.log(producto.includes('JavaScript'));
+console.log(producto.includes('Tablet'));
+
---
+++ js/05.js
@@ -0,0 +1,12 @@
+// NUMEROS
+
+const numero1 = 100;
+const numero2 = 200;
+const numero3 = 300;
+const numero4 = 8;
+const numero5 = 6;
+
+console.log(numero1 + numero2);
+console.log(numero2 - numero1);
+console.log(numero3 / numero1);
+console.log(numero4 % numero5);
