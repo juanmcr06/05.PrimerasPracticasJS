d566ec47499be861c769a74ffdbe167d004bf7a2
juanmcr06 <juanmcr.06@gmail.com>
2022-09-07 00:09:24 -0300

    update

--- js/25.js
+++ js/25.js
@@ -4,14 +4,53 @@
 //    console.log(i);                   // que deje de ser true la condicion (i<=10)
 //};
 
-for( let i = 1; i <= 10; i++) {
-    if(i % 2 === 0) {
-        console.log(`El numero ${i} es par`);
-    }
-}
+//for( let i = 1; i <= 10; i++) {
+//    if(i % 2 === 0) {
+//        console.log(`El numero ${i} es par`);
+//    }
+//}
+
+// const carrito = [
+    // {nombre: 'Monitor de 20 pulgadas', precio : 300},
+    // {nombre: 'Television de 50 pulgadas', precio : 500},
+    // {nombre: 'Tablet', precio : 200},
+    // {nombre: 'Audifonos', precio : 150},
+    // {nombre: 'Teclado', precio : 50},
+    // {nombre: 'Celular', precio : 500},
+    // {nombre: 'Parlantes', precio : 300},
+    // {nombre: 'Notebook', precio : 800},
+// ];
+
+// for (let i = 0; i < carrito.length; i++ ) {
+    // console.log(carrito[i].nombre, ' = ', carrito[i].precio);     // Envia a la consola cada elemento
+// };
 
 
 // WHILE LOOP
 
 
+// let i = 1;  // Indice o valor inicial
 
+// while(i < 10) { // Condicion
+
+//     if( i % 2 === 0) {
+//         console.log(`El numero ${i} es par`)
+//     }
+//     else {
+//         console.log(`El numero ${i} es impar`)
+//     }
+
+
+//     i++  // Incremento
+// }
+
+
+// DO WHILE
+
+let i = 0;
+
+do {
+
+}
+
+while ()
